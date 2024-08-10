a985e6e4c7dfcdfc9d9563a8224c74316dbb7799
HEINRICK7 <heinrickcostta@gmail.com>
2024-08-09 22:03:55 -0300

    update project

--- src/models/Paciente.ts
+++ src/models/Paciente.ts
@@ -4,14 +4,14 @@ export interface IPaciente extends Document {
   nome: string;
   email: string;
   telefone: string;
-  dataNascimento: Date;
+  dataNascimento: string;
 }
 
 const PacienteSchema: Schema = new Schema({
   nome: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   telefone: { type: String, required: true },
-  dataNascimento: { type: Date, required: true },
+  dataNascimento: { type: String, required: true },
 });
 
 export default mongoose.model<IPaciente>('Paciente', PacienteSchema);
