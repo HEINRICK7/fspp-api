0c72b714b8a4a7d8254379a4bf9bdaf002e75ade
HEINRICK7 <heinrickcostta@gmail.com>
2024-08-09 21:29:43 -0300

    update project

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const morgan = require("morgan");
 // Configure CORS para permitir localhost e a URL de produção
 app.use(
   cors({
-    origin: ["https://fspp-kmbm.vercel.app/api/v1/login"],
+    origin: ["*"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
@@ -20,7 +20,7 @@ app.use(morgan("dev"));
 // Responder a todas as requisições OPTIONS com CORS
 app.options("*", cors());
 
-app.use("/api/v1", authRoutes);
+app.use("/", authRoutes);
 app.use(errorHandler);
 
 export default app;
