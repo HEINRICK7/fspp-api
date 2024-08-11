c4671e8319c1c510e175a4898b2225f19f34ca21
HEINRICK7 <heinrickcostta@gmail.com>
2024-08-11 10:31:49 -0300

    update projects

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 require("dotenv").config();
-console.log("MONGO_URL:", process.env.MONGODB_URI);
+
 export const connectDB = async () => {
   try {
-    
     const mongoUri = process.env.MONGODB_URI;
-    console.log(mongoUri)
+    console.log(mongoUri);
     if (!mongoUri) {
       throw new Error("MONGODB_URI not defined in environment variables");
     }
-    await mongoose.connect(mongoUri || '');
+    await mongoose.connect(mongoUri || "");
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
