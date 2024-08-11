7751a89ae414556ce2209bdd1e2e415cb1288a89
HEINRICK7 <heinrickcostta@gmail.com>
2024-08-11 19:12:33 -0300

    update projects

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import morgan from "morgan";
 import { errorHandler } from "./middleware/errorMiddleware";
 import authRoutes from "./routes/authRoutes";
@@ -7,19 +7,25 @@ import pacienteRoutes from "./routes/pacienteRoutes";
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://fspp-git-master-heinrick7s-projects.vercel.app', // use your actual domain name (or localhost), using * is not recommended
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-  credentials: true
-}))
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://fspp-git-master-heinrick7s-projects.vercel.app"
+  ); // Update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 // Routes
 app.use("/api/v1", authRoutes);
-app.use('/api/v1', pacienteRoutes);
+app.use("/api/v1", pacienteRoutes);
 
 // Error handling
 app.use(errorHandler);
