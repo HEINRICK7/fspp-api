c4fbf643ae2d28029490b238084e4567b8a0ada1
HEINRICK7 <heinrickcostta@gmail.com>
2024-08-11 18:10:34 -0300

    update projects

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,37 @@
 import express from "express";
+import cors from 'cors';
+import morgan from "morgan";
 import { errorHandler } from "./middleware/errorMiddleware";
 import authRoutes from "./routes/authRoutes";
 import pacienteRoutes from "./routes/pacienteRoutes";
-import cors from 'cors'
 
 const app = express();
-const morgan = require("morgan");
+
 // Configure CORS para permitir localhost e a URL de produção
-app.use(cors({
-  origin: 'https://fspp-git-master-heinrick7s-projects.vercel.app',
-  methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-  credentials: true // Permite cookies de cross-site
-}));
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    const allowedOrigins = ['https://fspp-git-master-heinrick7s-projects.vercel.app', 'http://localhost:3000'];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-// Responder a todas as requisições OPTIONS com CORS
+
+// Routes
 app.use("/api/v1", authRoutes);
 app.use('/api/v1', pacienteRoutes);
+
+// Error handling
 app.use(errorHandler);
 
 export default app;
