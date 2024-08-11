1256f0595a5100c473321d03e029f660e14c177d
HEINRICK7 <heinrickcostta@gmail.com>
2024-08-11 18:38:32 -0300

    update projects

--- src/app.ts
+++ src/app.ts
@@ -7,22 +7,7 @@ import pacienteRoutes from "./routes/pacienteRoutes";
 
 const app = express();
 
-// Configure CORS para permitir localhost e a URL de produção
-const corsOptions = {
-  origin: function (origin: any, callback: any) {
-    const allowedOrigins = ['https://fspp-api.onrender.com', 'http://localhost:3000'];
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
