50dc55de13b860d26df2be1a10bb71cc6ec0bc4c
HEINRICK7 <heinrickcostta@gmail.com>
2024-08-11 17:48:17 -0300

    update projects

--- src/app.ts
+++ src/app.ts
@@ -7,20 +7,13 @@ const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 // Configure CORS para permitir localhost e a URL de produção
-app.use(
-  cors({
-    origin: ["*"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
-
+app.use(cors({
+  origin: 'https://fspp-git-master-heinrick7s-projects.vercel.app' // Apenas aceita requisições deste domínio
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 // Responder a todas as requisições OPTIONS com CORS
-app.options("*", cors());
-
 app.use("/api/v1", authRoutes);
 app.use('/api/v1', pacienteRoutes);
 app.use(errorHandler);
