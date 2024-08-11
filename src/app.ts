946113f01dd88fccec2e203290051e087a5bce92
HEINRICK7 <heinrickcostta@gmail.com>
2024-08-11 19:04:30 -0300

    update projects

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,12 @@ import pacienteRoutes from "./routes/pacienteRoutes";
 
 const app = express();
 
-const corsOptions = {
-  origin: "https://fspp-git-master-heinrick7s-projects.vercel.app", // Certifique-se de que este é exatamente o domínio de onde as requisições estão sendo feitas
-  methods: ['GET', 'POST', 'OPTIONS'], // Incluir OPTIONS é crucial para respostas de preflight
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204 // Alguns navegadores (e.g., Chrome) requerem status 204 para preflight
-};
-
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: 'https://fspp-git-master-heinrick7s-projects.vercel.app', // use your actual domain name (or localhost), using * is not recommended
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+  credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
@@ -24,10 +20,7 @@ app.use(morgan("dev"));
 // Routes
 app.use("/api/v1", authRoutes);
 app.use('/api/v1', pacienteRoutes);
-app.options('*', cors(corsOptions), (req, res) => {
-  console.log('Received OPTIONS request:', req.headers);
-  res.sendStatus(204);
-});
+
 // Error handling
 app.use(errorHandler);
 
